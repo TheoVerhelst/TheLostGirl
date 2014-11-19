2cd028f88862ba3a4f60a0e1bb4937a82ec49099
Théo Verhelst <tverhels@ulb.ac.be>
2014-11-19 15:10:18 +0100

    Remove save.json and executables files from git.

--- src/ContactListener.cpp
+++ src/ContactListener.cpp
@@ -4,31 +4,26 @@
 
 ContactListener::ContactListener(State::Context context):
 	m_context(context),
-	m_fallingListener(m_context),
-	m_actorIDListener(m_context)
+	m_fallingListener(m_context)
 {}
 
 
 void ContactListener::PreSolve(b2Contact* contact, const b2Manifold* oldManifold)
 {
 	m_fallingListener.PreSolve(contact, oldManifold);
-	m_actorIDListener.PreSolve(contact, oldManifold);
 }
 
 void ContactListener::BeginContact(b2Contact* contact)
 {
 	m_fallingListener.BeginContact(contact);
-	m_actorIDListener.BeginContact(contact);
 }
 
 void ContactListener::EndContact(b2Contact* contact)
 {
 	m_fallingListener.EndContact(contact);
-	m_actorIDListener.EndContact(contact);
 }
 
 void ContactListener::PostSolve(b2Contact* contact, const b2ContactImpulse* impulse)
 {
 	m_fallingListener.PostSolve(contact, impulse);
-	m_actorIDListener.PostSolve(contact, impulse);
 }
