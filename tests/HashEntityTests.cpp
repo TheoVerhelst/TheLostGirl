d16eae3707e52eb2986d5812b09a02df2d0181fd
TheoVerhelst <tverhels@ulb.ac.be>
2015-12-22 15:34:38 +0100

    Write tests for HashEntity.h

--- tests/HashEntityTests.cpp
+++ tests/HashEntityTests.cpp
@@ -1,9 +1,19 @@
 #include <boost/test/unit_test.hpp>
+#include <entityx/entityx.h>
+#include <TheLostGirl/components.h>
 #include <TheLostGirl/HashEntity.h>
 
 struct HashEntityTestsFixture
 {
-	HashEntityTestsFixture()
+	HashEntity hasher;
+	entityx::EventManager events;
+	entityx::EntityManager entities;
+	entityx::Entity entity1, entity2, entity3;
+	HashEntityTestsFixture():
+		entities(events),
+		entity1(entities.create()),
+		entity2(entities.create()),
+		entity3(entity1)
 	{
 	}
 
@@ -13,10 +23,20 @@ struct HashEntityTestsFixture
 };
 
 BOOST_FIXTURE_TEST_SUITE(HashEntityTests, HashEntityTestsFixture)
- 
-BOOST_AUTO_TEST_CASE(MyMethod)
+
+BOOST_AUTO_TEST_CASE(hash)
 {
-    BOOST_CHECK(1 + 1 == 2);
+	BOOST_CHECK_NE(hasher(entity1), hasher(entity2));
+	BOOST_CHECK_EQUAL(hasher(entity1), hasher(entity3));
+	entity1.assign<BodyComponent>();
+	BOOST_CHECK_NE(hasher(entity1), hasher(entity2));
+	BOOST_CHECK_EQUAL(hasher(entity1), hasher(entity3));
+	entity2.assign<BodyComponent>();
+	BOOST_CHECK_NE(hasher(entity1), hasher(entity2));
+	BOOST_CHECK_EQUAL(hasher(entity1), hasher(entity3));
+	entity1.assign<ItemComponent>();
+	BOOST_CHECK_NE(hasher(entity1), hasher(entity2));
+	BOOST_CHECK_EQUAL(hasher(entity1), hasher(entity3));
 }
- 
+
 BOOST_AUTO_TEST_SUITE_END()
