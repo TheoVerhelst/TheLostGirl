71c80d0145e7c34c8fe90b4e069d8ed1a4a17f91
TheoVerhelst <tverhels@ulb.ac.be>
2015-12-22 01:17:33 +0100

    Continue the implemetation of unit tests
    The tests of functions.h are almost finished.

--- tests/functionsTests.cpp
+++ tests/functionsTests.cpp
@@ -1,5 +1,6 @@
 #include <boost/test/unit_test.hpp>
 #include <entityx/entityx.h>
+#include <Box2D/Box2D.h>
 #include <TheLostGirl/components.h>
 #include <TheLostGirl/functions.h>
 
@@ -124,18 +125,90 @@ BOOST_AUTO_TEST_CASE(isPlayerTests)
 
 BOOST_AUTO_TEST_CASE(getBodyDefTests)
 {
+	b2World world({0.f, 1.f});
+
+	b2BodyDef def;
+	def.active = true;
+	def.allowSleep = true;
+	def.awake = true;
+	def.bullet = false;
+	def.fixedRotation = false;
+	def.angle = 2.f;
+	def.angularDamping = 1.f;
+	def.angularVelocity = 2.3f;
+	def.gravityScale = 1.3f;
+	def.linearDamping = 5.3f;
+	def.linearVelocity = {2.5f, 1.1f};
+	def.position = {55.f, 100.4f};
+	def.type = b2_dynamicBody;
+	def.userData = 0b0110101_vptr;
+
+	b2BodyDef newDef = getBodyDef(world.CreateBody(&def));
+	BOOST_CHECK_EQUAL(def.active, newDef.active);
+	BOOST_CHECK_EQUAL(def.allowSleep, newDef.allowSleep);
+	BOOST_CHECK_EQUAL(def.awake, newDef.awake);
+	BOOST_CHECK_EQUAL(def.bullet, newDef.bullet);
+	BOOST_CHECK_EQUAL(def.fixedRotation, newDef.fixedRotation);
+	BOOST_CHECK_EQUAL(def.userData, newDef.userData);
+	BOOST_CHECK_CLOSE(def.angle, newDef.angle, 0.0001);
+	BOOST_CHECK_CLOSE(def.angularDamping, newDef.angularDamping, 0.0001);
+	BOOST_CHECK_CLOSE(def.angularVelocity, newDef.angularVelocity, 0.0001);
+	BOOST_CHECK_CLOSE(def.gravityScale, newDef.gravityScale, 0.0001);
+	BOOST_CHECK_CLOSE(def.linearDamping, newDef.linearDamping, 0.0001);
+	BOOST_CHECK_CLOSE(def.linearVelocity.x, newDef.linearVelocity.x, 0.0001);
+	BOOST_CHECK_CLOSE(def.linearVelocity.y, newDef.linearVelocity.y, 0.0001);
+	BOOST_CHECK_CLOSE(def.position.x, newDef.position.x, 0.0001);
+	BOOST_CHECK_CLOSE(def.position.y, newDef.position.y, 0.0001);
 }
 
 BOOST_AUTO_TEST_CASE(getFixtureDefTests)
 {
+	b2World world({0.f, 1.f});
+	b2BodyDef bodyDef;
+	b2Body* body = world.CreateBody(&bodyDef);
+
+	b2PolygonShape shape;
+	shape.SetAsBox(2.f, 5.f);
+
+	b2Filter filter;
+	filter.categoryBits = 0b0101u;
+	filter.groupIndex = 3;
+	filter.maskBits = 0b10001u;
+
+	b2FixtureDef def;
+	def.isSensor = false;
+	def.density = 0.312f;
+	def.friction = 0.949f;
+	def.restitution = 0.123f;
+	def.filter = filter;
+	def.userData = 0b0110101_vptr;
+	def.shape = &shape;
+
+	b2FixtureDef newDef = getFixtureDef(body->CreateFixture(&def));
+	BOOST_CHECK_EQUAL(def.isSensor, newDef.isSensor);
+	BOOST_CHECK_EQUAL(def.userData, newDef.userData);
+	BOOST_CHECK_EQUAL(def.filter.categoryBits, newDef.filter.categoryBits);
+	BOOST_CHECK_EQUAL(def.filter.groupIndex, newDef.filter.groupIndex);
+	BOOST_CHECK_EQUAL(def.filter.maskBits, newDef.filter.maskBits);
+	BOOST_CHECK_EQUAL(def.shape->m_type, newDef.shape->m_type);
+	BOOST_CHECK_CLOSE(def.shape->m_radius, newDef.shape->m_radius, 0.0001);
+	BOOST_CHECK_CLOSE(def.density, newDef.density, 0.0001);
+	BOOST_CHECK_CLOSE(def.friction, newDef.friction, 0.0001);
+	BOOST_CHECK_CLOSE(def.restitution, newDef.restitution, 0.0001);
 }
 
 BOOST_AUTO_TEST_CASE(getJointDefTests)
 {
+	//TODO Implements this test (alot of code)
 }
 
 BOOST_AUTO_TEST_CASE(operatorNotTests)
 {
+	BOOST_CHECK((not Direction::Left) == Direction::Right);
+	BOOST_CHECK((not Direction::Right) == Direction::Left);
+	BOOST_CHECK((not Direction::Top) == Direction::Bottom);
+	BOOST_CHECK((not Direction::Bottom) == Direction::Top);
+	BOOST_CHECK((not Direction::None) == Direction::None);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
