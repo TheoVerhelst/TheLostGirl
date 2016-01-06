cf3b0166fe9a941232e1037fd2563c4ed8a61446
TheoVerhelst <tverhels@ulb.ac.be>
2016-01-06 14:21:31 +0100

    Remove useless try/catch blocks in main()
    The try/catch blocks that were there were useless because they do
    nothing more than if they were not present.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #if defined(__linux__)
 	#include <X11/Xlib.h>//Thread init
 	#undef None//The X11 library define None as 0 so undef it in order to don't conflict with some None in enums
-	#undef Status
+	#undef Status//Same as None above
 #endif//defined(__linux__)
 
 #include <TheLostGirl/Application.hpp>
@@ -12,17 +12,9 @@ int main()
 	XInitThreads();//For multithreadings on linux, must be called before the initialisation of the StateStack
 #endif//defined(__linux__)
 
-	try
-	{
 #ifdef NDEBUG
-		Application(false).run();
+	Application(false).run();
 #else
-		Application(true).run();
+	Application(true).run();
 #endif
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << "Exception: " << e.what() << std::endl;
-		return EXIT_FAILURE;
-	}
 }
