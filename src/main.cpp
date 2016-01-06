5eede78a8c65d8255fac29d08d024f6dd05b5d18
TheoVerhelst <tverhels@ulb.ac.be>
2016-01-06 14:17:49 +0100

    Use preprocessor to conditionnaly include X11
    The X11 library must be included in order to call XInitThreads() before
    any use of threads, and these changes automatically disable this part of
    code if the program is not compilud with linux.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
-#include <X11/Xlib.h>//Thread init
-#undef None//The X11 library define None as 0 so undef it in order to don't conflict with some None in enums
-#undef Status
+#if defined(__linux__)
+	#include <X11/Xlib.h>//Thread init
+	#undef None//The X11 library define None as 0 so undef it in order to don't conflict with some None in enums
+	#undef Status
+#endif//defined(__linux__)
+
 #include <TheLostGirl/Application.hpp>
 
 int main()
 {
+#if defined(__linux__)
 	XInitThreads();//For multithreadings on linux, must be called before the initialisation of the StateStack
+#endif//defined(__linux__)
+
 	try
 	{
 #ifdef NDEBUG
