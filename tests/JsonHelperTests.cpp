810587bd9f6462eb4dd8781367fa94c243937d09
TheoVerhelst <tverhels@ulb.ac.be>
2016-01-27 10:55:08 +0100

    Add missing test file to git
    The file JsonHelperTests.cpp were listed in CMakeLists.txt but not in
    the git repository. This is fixed.

---
+++ tests/JsonHelperTests.cpp
@@ -0,0 +1,18 @@
+#include <boost/test/unit_test.hpp>
+#include <TheLostGirl/Player.hpp>
+#include <TheLostGirl/systems.hpp>
+
+struct JsonHelperTestsFixture
+{
+	JsonHelperTestsFixture()
+	{
+	}
+
+	~JsonHelperTestsFixture()
+	{
+	}
+};
+
+BOOST_FIXTURE_TEST_SUITE(JsonHelperTests, JsonHelperTestsFixture)
+
+BOOST_AUTO_TEST_SUITE_END()
