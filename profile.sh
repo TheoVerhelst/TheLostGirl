3a00e0530b2878b4b0627a60c0fbe55011a72513
TheoVerhelst <tverhels@ulb.ac.be>
2016-04-26 11:09:37 +0200

    Allow to build the documentation with CMake

--- profile.sh
+++ profile.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 # Profiling script for The Lost Girl.
 #
-# This script assumes that an executable called TheLostGirl is located
-# in the current directory, and a profiling output called gmon.out is
-# also located in the current directory. This script also use python3
-# for the truncating feature.
-# 
-# The executable have to be compiled with the option -pg if this is
-# g++, or any other option that makes a profile output suitable for
-# use with gprof.
+# This script assumes that an executable called TheLostGirl and a profiling
+# output called gmon.out are located in $1 (the first command line argument).
+# This script uses python3 for the truncating feature.
 #
-# The trucating option truncate the output file such that only 145
-# characters are displayed on the screen. This is more readable when
-# very long template names are used.
+# The executable have to be compiled with the option -pg if this is g++, or any
+# other option that makes a profile output suitable for use with gprof.
 #
+# The trucating option truncates the output file such that only 145 characters
+# are displayed on the screen. This is more readable when very long template
+# names are in use.
 
 print_usage()
 {
